df4a7faea0f5198c66f4e38614226fd113da19fb
Alisa <alisa.luoma@hotmail.fi>
2023-01-20 12:45:15 +0200

    First challenge

---
+++ notes-app/app.js
@@ -0,0 +1,9 @@
+const fs = require('fs')
+
+fs.writeFileSync('notes.txt', 'My name is Alisa Luomanmäki.')
+
+try {
+    fs.appendFileSync('notes.txt', 'Apped to file')
+} catch (err) {
+    console.log('Error has found')
+}
