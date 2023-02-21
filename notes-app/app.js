bb4fb57829befdb52aab8d95159eab4aa831a69d
Alisa <alisa.luoma@hotmail.fi>
2023-02-21 13:02:18 +0200

    Little data

--- notes-app/app.js
+++ notes-app/app.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 fs.writeFileSync('notes.txt', 'My name is Alisa Luomanmäki.')
 
 try {
-    fs.appendFileSync('notes.txt', 'Apped to file')
+    fs.appendFileSync('notes.txt', ' Apped to file')
 } catch (err) {
     console.log('Error has found')
 }
